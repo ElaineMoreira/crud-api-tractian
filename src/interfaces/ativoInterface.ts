f106e196a7a8203fb7ba499ba23dbeda4c7dc160
Elaine <elainefelipemoreira@gmail.com>
2022-04-16 13:48:27 -0300

    feat: adiciona atributos na interface ativoInterface.ts

--- src/interfaces/ativoInterface.ts
+++ src/interfaces/ativoInterface.ts
@@ -1,3 +1,13 @@
 export interface ativoInterface {
-
+  imagem: string,
+  nome: string,
+  descricao: string,
+  modelo: string,
+  proprietario: string,
+  status:{
+    execucao: string
+    alerta: string
+    parado: string
+  },
+  nivelDeIntegridade: number
 }
