692258d39c4784c4fa291e87899a74bf56588002
Elaine <elainefelipemoreira@gmail.com>
2022-04-16 10:07:08 -0300

    docs: Adiciona configuracao completa para subir servidor na porta escolhida

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { Response, Request } from 'express'
+
+const app = express()
+
+app.get('./', (req: Request, res: Response) => {
+  return res.send('Oi Elaine, só pra testar!')
+})
+
+app.listen(3333, () => console.log(`Servidor da Elaine Online na porta 3333!`))
