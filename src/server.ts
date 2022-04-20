314823f77ee38095803709b1ef51aecc7e01cddf
Elaine <elainefelipemoreira@gmail.com>
2022-04-20 17:48:39 -0300

    refactor: altera conexao no server

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,10 @@ app.use(router)
 
 const SENHA_SECRETA_ENV = process.env.SECRET_CONNECTION_MONGO
 // const PORT = process.env.PORT
-const PORT = 3003 || process.env.PORT
+const PORT = process.env.PORT || 3003
 
-mongoose.connect('mongodb+srv://elainemoreira:trybe12345@cluster0.lev3k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-// mongoose.connect(`${SENHA_SECRETA_ENV}`)
+// mongoose.connect('mongodb+srv://elainemoreira:trybe12345@cluster0.lev3k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(`${SENHA_SECRETA_ENV}`)
 /** se deu certo .*/
   .then((data) => {
     console.log("Conexão bem sucedida do MongoDB!", data.version)
